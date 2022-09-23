324dad920391e7131adfe145b721addd43140f89
Markus Staab <m.staab@complex-it.de>
2022-09-23 17:11:15 +0200

    simplify further

--- application/fare.de/phpstan-bootstrap.php
+++ application/fare.de/phpstan-bootstrap.php
@@ -1,11 +1,4 @@
 <?php
 
-define('APP_ROOT', dirname(__DIR__, 2));
-define('APP_NAME', basename(__DIR__));
-define('APP_PATH', APP_ROOT.'/application/'.APP_NAME);
-define('PUBLIC_PATH', APP_ROOT.'/public/'.APP_NAME);
 define('APPLICATION_ROOT', APP_ROOT);
-
-$_SERVER['APP_MODE'] = 'development';
-define('APP_MODE', $_SERVER['APP_MODE']);
 
